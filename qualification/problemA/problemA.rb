b56e7a88b581ef782400e43656bbc2eeed9a81f4
Jack Hoy <jackhoy05@gmail.com>
2014-04-12 18:01:38 +0100

    small refactoring

--- qualification/problemA/problemA.rb
+++ qualification/problemA/problemA.rb
@@ -10,18 +10,18 @@ file = File.new("output.txt", "w")
 until game_number > total_games_to_play
   first_row_number = input[line_number].to_i
   second_row_number = input[line_number + 5].to_i
+
   first_numbers = input[line_number + first_row_number].split(%r{\D})
   second_numbers = input[line_number + 5 + second_row_number].split(%r{\D})
   result = first_numbers.delete_if {|n| !second_numbers.include? n }
-  print result
-  if result.length == 0
-    file.puts "Case ##{game_number}: Volunteer cheated!"
-  elsif result.length == 1
-    file.puts "Case ##{game_number}: #{result[0]}"
-  else
-    file.puts "Case ##{game_number}: Bad magician!"
+
+  case result.length
+  when 0 ; file.puts "Case ##{game_number}: Volunteer cheated!"
+  when 1 ; file.puts "Case ##{game_number}: #{result[0]}"
+  else   ; file.puts "Case ##{game_number}: Bad magician!"
   end
 
+  # move to next game
   line_number += 10
   game_number += 1
 end
