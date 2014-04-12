16a07d16310e2b32f65776bf10c10f2eba8169cd
Jack Hoy <jackhoy05@gmail.com>
2014-04-12 21:02:29 +0100

    solved problem b for the small input

--- qualification/problemB/problemB.rb
+++ qualification/problemB/problemB.rb
@@ -12,22 +12,27 @@ until game_number > total_games_to_play
   @cookie_farm_cost = game_data[0].to_f
   @cookie_farm_volume = game_data[1].to_f
   @target_cookies = game_data[2].to_f
-  @total_cookies = 0.0
+  @current_cookies = 0.0
   @total_time = 0.0
 
   def better_to_buy?
-    time_with_new_farm = @total_cookies / (@production_level + @cookie_farm_volume)
-    time_without_new_farm = (@target_cookies - @total_cookies) / @production_level
+    time_with_new_farm = @target_cookies / (@production_level + @cookie_farm_volume)
+    time_without_new_farm = (@target_cookies - @current_cookies) / @production_level
     time_with_new_farm < time_without_new_farm
   end
 
-  until @total_cookies >= @target_cookies
-    if (@total_cookies >= @cookie_farm_cost) && better_to_buy?
-      @total_cookies -= @cookie_farm_cost
-      @production_level += @cookie_farm_volume
+  until @current_cookies >= @target_cookies
+    if @current_cookies < @cookie_farm_cost
+      @total_time += (@cookie_farm_cost - @current_cookies) / @production_level
+      @current_cookies += @cookie_farm_cost
+      if better_to_buy?
+        @current_cookies -= @cookie_farm_cost
+        @production_level += @cookie_farm_volume
+      else
+        @total_time += (@target_cookies - @current_cookies) / @production_level
+        @current_cookies = @target_cookies
+      end
     end
-    @total_cookies += @production_level
-    @total_time += 1
   end
 
   file.puts "Case ##{game_number}: #{@total_time}"
